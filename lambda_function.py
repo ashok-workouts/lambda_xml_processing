51aadce658788b4e2c2a66881e0885439b06af94
Chunduru <aschunduru@galaxe.com>
2023-07-13 19:13:20 +0530

    modified the main function

--- lambda_function.py
+++ lambda_function.py
@@ -67,7 +67,7 @@ def shape_data(tab, file):
 
 
 def lambda_handler(event, context):
-    print("Lambda execution started.....rom lambda")
+    print("Lambda execution started from lambda.....")
     file_name = get_latest_file(bucket_name, prefix)
     file_extension = pathlib.Path(file_name).suffix
     tab = file_name.split('.')[0]
